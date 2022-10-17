7ab31000298a9ca53114f060941838fa5fbcabf4
Trung Ngo <codem01@gmail.com>
2022-10-16 21:40:27 -0500

    Add Windows file tagging

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Stable Difussion Web UI Tools
 
 Various python and nodeJS scripts that are useful with this fork of
-["stable-diffusion-webui"](https://github.com/tsngo/stable-diffusion-webui).
+[stable-diffusion-webui](https://github.com/tsngo/stable-diffusion-webui).
 
 Most tools will depend on the JSON parameter file output unless otherwise noted.
 
+# Installation
+pip install -r requirements.txt
+
+# Usage
+
 ## Strip PNG Chunk Info
-python ./strip_png_info.py /path/to/file [/path/to/newfile]
+python strip_png_info.py /path/to/file [/path/to/newfile]
 
 Removes the PNG Check Info from a PNG file
 
 ## Get PNG Chunk Info
-python ./get_png_info.py /path/to/file
+python get_png_info.py /path/to/file
 
 Outputs the PNG Chunk Info from a PNG file as JSON
+
+## Add Tags to file (Windows only)
+python add_tags.py /path/to/file csv_of_tags [cvs_tags_to_remove]
+
+Uncomment pywin32 in requirements.txt and rerun installation steps. Adds list of tags and removes list of tags for a file that supports tags. Recommend using [FileMeta](https://github.com/Dijji/FileMeta/releases) to allow PNG file tags.
---
+++ add_tags.py
@@ -0,0 +1,44 @@
+import sys
+# requires pywin32 (See requirements.txt. Supports Windows only obviously)
+# also recommend FileMeta https://github.com/Dijji/FileMeta to allow tags for PNG
+import pythoncom
+from win32com.propsys import propsys
+from win32com.shell import shellcon
+import os
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+try:
+    filename = os.path.realpath(sys.argv[1])
+    tags = sys.argv[2].split(',')
+    if len(sys.argv) == 4:
+        remove_tags = sys.argv[3].split(',')
+except:
+    print(f"python {__file__} /path/to/file csv_of_tags [cvs_tags_to_remove]")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    # get PROPERTYKEY for "System.Keywords"
+    pk = propsys.PSGetPropertyKeyFromName("System.Keywords")
+
+    # get property store for a given shell item (here a file)
+    ps = propsys.SHGetPropertyStoreFromParsingName(
+        filename, None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore)
+
+    # read & print existing (or not) property value, System.Keywords type is an array of string
+    existingTags = ps.GetValue(pk).GetValue()
+    if existingTags==None:
+        existingTags=[]
+    filteredTags = []
+
+    for tag in existingTags:
+        if tag in remove_tags:
+            continue
+        filteredTags.append(tag)
+
+    # build an array of string type PROPVARIANT
+    newValue = propsys.PROPVARIANTType(
+        filteredTags + tags, pythoncom.VT_VECTOR | pythoncom.VT_BSTR)
+
+    # write property
+    ps.SetValue(pk, newValue)
+    ps.Commit()
