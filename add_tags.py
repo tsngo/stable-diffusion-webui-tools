40ae7760360296125a2025d94cca259e9a5268a0
Trung Ngo <codem01@gmail.com>
2022-10-16 23:54:44 -0500

    switch arg parsing for add tags and allow tagging multiple files

--- add_tags.py
+++ add_tags.py
@@ -5,41 +5,62 @@ import pythoncom
 from win32com.propsys import propsys
 from win32com.shell import shellcon
 import os
+import argparse
+import glob
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
+def tag_files(files_glob, tags=[], remove_tags=[], remove_all_tags=False):
+    files = glob.glob(files_glob)
+    for file in files:
+        # get PROPERTYKEY for "System.Keywords"
+        pk = propsys.PSGetPropertyKeyFromName("System.Keywords")
+
+        try:
+            # get property store for a given shell item (here a file)
+            ps = propsys.SHGetPropertyStoreFromParsingName(
+                os.path.realpath(file), None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore)
+
+            # read & print existing (or not) property value, System.Keywords type is an array of string
+            existingTags = ps.GetValue(pk).GetValue()
+            if existingTags == None:
+                existingTags = []
+            filteredTags = []
+
+            if not remove_all_tags:
+                for tag in existingTags:
+                    if tag in remove_tags:
+                        continue
+                    filteredTags.append(tag)
+
+            # build an array of string type PROPVARIANT
+            newValue = propsys.PROPVARIANTType(
+                filteredTags + tags, pythoncom.VT_VECTOR | pythoncom.VT_BSTR)
+
+            # write property
+            ps.SetValue(pk, newValue)
+            ps.Commit()
+        except:
+            print(f"Unable to tag file {file}. Tags may not be supported for this file type.")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--files-glob", type=str, default="", help="glob pattern to files to tag", required=True)
+parser.add_argument("-t", "--tags", type=str, default="", help="comma separated list of tags", required=False)
+parser.add_argument("-r", "--remove-tags", type=str, default="", help="comma separated list of tags to remove", required=False)
+parser.add_argument("-rt", "--remove-all-tags", action="store_true", help="remove all tags", required=False)
+
 if __name__ == "__main__":
-    try:
-        filename = os.path.realpath(sys.argv[1])
-        tags = sys.argv[2].split(',')
-        if len(sys.argv) == 4:
-            remove_tags = sys.argv[3].split(',')
-    except:
-        print(f"python {__file__} /path/to/file csv_of_tags [cvs_tags_to_remove]")
-        sys.exit(1)
+    args = parser.parse_args()
     
-    # get PROPERTYKEY for "System.Keywords"
-    pk = propsys.PSGetPropertyKeyFromName("System.Keywords")
+    if args.tags == "":
+        args.tags = []
+    else:
+        args.tags = args.tags.split(',')
 
-    # get property store for a given shell item (here a file)
-    ps = propsys.SHGetPropertyStoreFromParsingName(
-        filename, None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore)
-
-    # read & print existing (or not) property value, System.Keywords type is an array of string
-    existingTags = ps.GetValue(pk).GetValue()
-    if existingTags==None:
-        existingTags=[]
-    filteredTags = []
-
-    for tag in existingTags:
-        if tag in remove_tags:
-            continue
-        filteredTags.append(tag)
-
-    # build an array of string type PROPVARIANT
-    newValue = propsys.PROPVARIANTType(
-        filteredTags + tags, pythoncom.VT_VECTOR | pythoncom.VT_BSTR)
-
-    # write property
-    ps.SetValue(pk, newValue)
-    ps.Commit()
+    if args.remove_tags == "":
+        args.remove_tags = []
+    else:
+        args.remove_tags = args.remove_tags.split(',')
+    
+    tag_files(**args.__dict__)
