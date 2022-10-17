d0ccdeace0f89d44aea2639e7e1c6f83fd4c4c6b
Trung Ngo <codem01@gmail.com>
2022-10-17 11:47:13 -0500

    check pywin32 dependencies

--- add_tags.py
+++ add_tags.py
@@ -2,8 +2,13 @@ import sys
 # requires pywin32 (See requirements.txt. Supports Windows only obviously)
 # also recommend FileMeta https://github.com/Dijji/FileMeta to allow tags for PNG
 import pythoncom
-from win32com.propsys import propsys
-from win32com.shell import shellcon
+try:
+    from win32com.propsys import propsys
+    from win32com.shell import shellcon
+except:
+    propsys = None
+    shellcon = None
+
 import os
 import argparse
 import glob
@@ -11,41 +16,44 @@ import glob
 script_path = os.path.dirname(os.path.realpath(__file__))
 
 def tag_files(files_glob="", tags=[], remove_tags=[], remove_all_tags=False, filename=""):
+    if propsys == None or shellcon == None:
+        return
+
     if files_glob=="":
         files = [filename]
     else:
         files = glob.glob(files_glob)
 
     for file in files:
-        # get PROPERTYKEY for "System.Keywords"
+        # get property store for a given shell item (here a file)
+        try:
+            ps = propsys.SHGetPropertyStoreFromParsingName(os.path.realpath(file), None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore)
+        except:
+            pythoncom.CoInitialize()
+            ps = propsys.SHGetPropertyStoreFromParsingName(os.path.realpath(file), None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore)
+            
         pk = propsys.PSGetPropertyKeyFromName("System.Keywords")
 
-        try:
-            # get property store for a given shell item (here a file)
-            ps = propsys.SHGetPropertyStoreFromParsingName(
-                os.path.realpath(file), None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore)
+        # read & print existing (or not) property value, System.Keywords type is an array of string
+        existingTags = ps.GetValue(pk).GetValue()
+        if existingTags == None:
+            existingTags = []
+        filteredTags = []
 
-            # read & print existing (or not) property value, System.Keywords type is an array of string
-            existingTags = ps.GetValue(pk).GetValue()
-            if existingTags == None:
-                existingTags = []
-            filteredTags = []
+        if not remove_all_tags:
+            for tag in existingTags:
+                if tag in remove_tags:
+                    continue
+                filteredTags.append(tag)
 
-            if not remove_all_tags:
-                for tag in existingTags:
-                    if tag in remove_tags:
-                        continue
-                    filteredTags.append(tag)
+        # build an array of string type PROPVARIANT
+        newValue = propsys.PROPVARIANTType(
+            filteredTags + tags, pythoncom.VT_VECTOR | pythoncom.VT_BSTR)
 
-            # build an array of string type PROPVARIANT
-            newValue = propsys.PROPVARIANTType(
-                filteredTags + tags, pythoncom.VT_VECTOR | pythoncom.VT_BSTR)
+        # write property
+        ps.SetValue(pk, newValue)
+        ps.Commit()
 
-            # write property
-            ps.SetValue(pk, newValue)
-            ps.Commit()
-        except:
-            print(f"Unable to tag file {file}. Tags may not be supported for this file type.")
 
 
 parser = argparse.ArgumentParser()
